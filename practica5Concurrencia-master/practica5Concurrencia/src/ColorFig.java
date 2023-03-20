476b1faed176c8eb68550ba3cab3e8d924155ba7
huang <huang@LAPTOP-EBFTU7OL>
2023-03-20 23:12:13 +0100

    parte javadoc de color

--- practica5Concurrencia-master/practica5Concurrencia/src/ColorFig.java
+++ practica5Concurrencia-master/practica5Concurrencia/src/ColorFig.java
@@ -19,70 +19,59 @@
 // General Public License for more details.                          //
 ///////////////////////////////////////////////////////////////////////
 
-
-
 import java.awt.Color;
 
 /**
- * ColorFig por Michael Gonzalez y Mariano Benito. 
- * Version 3.5. Octubre 2016. <p>
- * Clase que define algunos colores usados en la clase Dibujo <p>
- *
- * @author Mariano Benito Hoz <mbenitohoz at gmail dot com>
- * @version 3.4
+ * @author sofia
+ * @version 1.0
+ * 
+ *          Crear una figura que puede ajustar su color
+ * 
  */
 public class ColorFig {
 
-    private Color col;
+	private Color col;
 
-    // Constructor privado, que obtiene el color
-    //   a partir de un objeto de la clase Color
-    private ColorFig(Color col) {
-        this.col = new Color(col.getRed(), col.getGreen(), col.getBlue());
-    }
+	// Constructor privado, que obtiene el color
+	// a partir de un objeto de la clase Color
+	/**
+	 * @param el color que quieres tener el figura creado
+	 */
+	private ColorFig(Color col) {
+		this.col = new Color(col.getRed(), col.getGreen(), col.getBlue());
+	}
 
-    // Retorna el color en un objeto de la clase Color
-    Color colorOf() {
-        return col;
-    }
+	// Retorna el color en un objeto de la clase Color
+	/**
+	 * @return el color de la figura
+	 */
+	Color colorOf() {
+		return col;
+	}
 
-    /**
-     * Contructor que crea un color a partir de una combinacion
-     * de rojo (r) verde (g) y azul (b). Cada uno de estos datos
-     * estara comprendido entre 0 y 255
-     */
-    public ColorFig(int r, int g, int b) {
-        this.col = new Color(r, g, b);
-    }
+	public ColorFig(int r, int g, int b) {
+		this.col = new Color(r, g, b);
+	}
 
-    /** 
-     * Constantes estaticas para los colores mas habituales 
-     */
-    public static final ColorFig negro = new ColorFig(Color.black);
-    public static final ColorFig azul = new ColorFig(Color.blue);
-    public static final ColorFig grisOscuro = new ColorFig(Color.darkGray);
-    public static final ColorFig gris = new ColorFig(Color.gray);
-    public static final ColorFig verde = new ColorFig(Color.green);
-    public static final ColorFig grisClaro = new ColorFig(Color.lightGray);
-    public static final ColorFig magenta = new ColorFig(Color.magenta);
-    public static final ColorFig naranja = new ColorFig(Color.orange);
-    public static final ColorFig rosa = new ColorFig(Color.pink);
-    public static final ColorFig rojo = new ColorFig(Color.red);
-    public static final ColorFig blanco = new ColorFig(Color.white);
-    public static final ColorFig amarillo = new ColorFig(Color.yellow);
+	public static final ColorFig negro = new ColorFig(Color.black);
+	public static final ColorFig azul = new ColorFig(Color.blue);
+	public static final ColorFig grisOscuro = new ColorFig(Color.darkGray);
+	public static final ColorFig gris = new ColorFig(Color.gray);
+	public static final ColorFig verde = new ColorFig(Color.green);
+	public static final ColorFig grisClaro = new ColorFig(Color.lightGray);
+	public static final ColorFig magenta = new ColorFig(Color.magenta);
+	public static final ColorFig naranja = new ColorFig(Color.orange);
+	public static final ColorFig rosa = new ColorFig(Color.pink);
+	public static final ColorFig rojo = new ColorFig(Color.red);
+	public static final ColorFig blanco = new ColorFig(Color.white);
+	public static final ColorFig amarillo = new ColorFig(Color.yellow);
 
-    /**
-     *  Retorna un color mas oscuro que el original
-     */
-    public ColorFig masOscuro() {
-        return new ColorFig(col.darker());
-    }
+	public ColorFig masOscuro() {
+		return new ColorFig(col.darker());
+	}
 
-    /**
-     * Retorna un color mas claro que el original
-     */
-    public ColorFig masClaro() {
-        return new ColorFig(col.brighter());
-    }
+	public ColorFig masClaro() {
+		return new ColorFig(col.brighter());
+	}
 
 }
